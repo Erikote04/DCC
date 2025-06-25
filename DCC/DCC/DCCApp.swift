d01ef02f53bc36d6258b186f7b9b39b3dc9b8dfa
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-25 13:28:42 +0200

    chore(project): init

---
+++ DCC/DCC/DCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCCApp.swift
+//  DCC
+//
+//  Created by Erik Sebastian de Erice Jerez on 25/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
