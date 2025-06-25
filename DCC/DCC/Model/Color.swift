4fa3116ca1e3d262c066dc7ce6218cec48ef0be2
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-25 13:38:21 +0200

    chore(data): created Color model

---
+++ DCC/DCC/Model/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  DCC
+//
+//  Created by Erik Sebastian de Erice Jerez on 25/6/25.
+//
+
+import SwiftUI
+
+struct Color: Identifiable {
+    let id: Int
+    let name: String
+    let color: SwiftUI.Color
+    let hex: String
+    let combinations: [Int]
+    let swatchCollection: Int
+}
